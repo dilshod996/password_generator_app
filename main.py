b2b5d4d167ca5b342bb7fee4aed2392e14dc37fb
dilshod996 <dnurmamatoff@gmail.com>
2022-04-16 21:52:04 +0900

    Added Json file

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import json
 
-FONT =("Arial", 8, "bold")
+FONT = ("Arial", 8, "bold")
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    new_data = {
+        website_entry.get(): {"email": user_entry.get(),
+                              "password": space_password.get()}
+    }
+    if len(website_entry.get()) == 0 or len(space_password.get()) == 0 or len(user_entry.get()) == 0:
+        messagebox.showinfo(title="Hey yooo", message="Don't forget to fill all blanks !!!")
+    else:
+        try:
+            with open("data.json", "r") as json_file:
+                # json.dump(new_data, json_file, indent=4)
+                my_data = json.load(json_file)
+                my_data.update(new_data)
+        except FileNotFoundError:
+            with open("data.json", "w") as json_file:
+                json.dump(new_data, json_file, indent=4)
+        else:
+            with open("data.json", "w") as json_file:
+                json.dump(my_data, json_file, indent=4)
+
+
+            website_entry.delete(0, END)
+            space_password.delete(0, END)
+
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -27,48 +58,36 @@ def password_generate():
     space_password.insert(0, string_password)
 
 
+def search_engine():
+
+    enter_web= website_entry.get()
 
 
-
-
-
-# ---------------------------- SAVE PASSWORD ------------------------------- #
-def save():
-
-
-    if len(website_entry.get()) == 0 or len(space_password.get())==0 or len(user_entry.get())==0:
-        messagebox.showinfo(title="Hey yooo", message="Don't forget to fill all blanks !!!")
-    else:
-        is_ok = messagebox.askokcancel(title=website_entry.get(), message=f"These are all details: \nEmail:{user_entry.get()}\nPassword:{space_password.get()}")
-        if is_ok:
-            file_name = open("data.txt", "a")
-            file_name.write(f"{website_entry.get()} | {user_entry.get()} | {space_password.get()}"+"\n")
-            file_name.close()
-            website_entry.delete(0, END)
-            space_password.delete(0, END)
-
-
-
-
+    with open("data.json", "r") as my_file:
+        all_data = json.load(my_file)
+        try:
+            messagebox.showinfo(title="Information", message=f"Email: {all_data[enter_web]['email']}\n Password: {all_data[enter_web]['password']}")
+        except KeyError:
+            messagebox.showinfo(title="NOT REGISTERED", message="You have not still registered it")
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
-window.config(pady=50, padx=50,)
+window.config(pady=50, padx=50, )
 
 # Canvas
-my_canvas = Canvas(width=200, height= 200, highlightthickness=0)
+my_canvas = Canvas(width=200, height=200, highlightthickness=0)
 logo_img = PhotoImage(file="logo.png")
 
 # Image
-my_canvas.create_image(90, 100, image= logo_img)
+my_canvas.create_image(90, 100, image=logo_img)
 my_canvas.grid(row=0, column=1)
 
 name_website = Label(text="Website: ")
 name_website.grid(row=1, column=0)
 
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry.grid(row=1, column=1, sticky="EW")
 website_entry.get()
 
 name_user = Label(text="Email/Username:")
@@ -84,11 +103,15 @@ name_password.grid(row=3, column=0)
 space_password = Entry(width=21)
 space_password.grid(row=3, column=1, sticky="EW")
 
-gen_button = Button(text="Generate", highlightthickness=0, command=password_generate,)
+gen_button = Button(text="Generate", highlightthickness=0, command=password_generate, )
 gen_button.grid(row=3, column=2, sticky="EW")
 
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
+
+search_button = Button(text="Search", command=search_engine)
+search_button.grid(row=1, column=2, sticky="EW")
+
 
 
 
